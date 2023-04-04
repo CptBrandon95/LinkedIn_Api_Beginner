e4d20bbb15093fb21aad75da0016d9b7b02d31ec
brandon wiener <brandon.wiener@redacademy.co.za>
2023-04-04 15:57:55 +0200

    Filtering through products as well as skipping through products

---
+++ LinkedIn_Api_Beginner/Models/QueryParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace LinkedIn_Api_Beginner.Models
+{
+    /*
+     A query class to handle more advanced data retrievals
+     */
+    public class QueryParameters
+    {
+        const int _maxSize = 100;
+        private int _size = 50;
+        public int Page { get; set; } = 1; // Initializing the page 1 which is our default page
+        public int Size 
+        {
+            get
+            { return _size; }
+            set 
+            { _size = Math.Min(_maxSize, value); }
+        }
+    }
+}
